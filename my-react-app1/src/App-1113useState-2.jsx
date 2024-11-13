86664a562be591f9fb397ed798ad1e22b642217e
season-123-docom <bobo10161995@gmail.com>
2024-11-13 17:17:21 +0800

    Add files via upload

--- my-react-app1/src/App-1113useState-2.jsx
+++ my-react-app1/src/App-1113useState-2.jsx
@@ -1,6 +1,4 @@
-//記得清空存檔，vite只能執行這個頁面的樣子
-
-//20241113上課進度3
+//20241113上課進度2
 import {useState} from "react"
 
 function App(){
@@ -11,7 +9,7 @@ function App(){
 
 
   function changeName(e){
-    setName2(e.target.value);
+    setName(e.target.value)
   }
  
   return(
@@ -19,10 +17,10 @@ function App(){
       <h2>設計一個輸入名字欄位，並且即時顯示出來</h2>
       <hr/>
       <h3>目前文字方塊的內容:{name}</h3>
-      請輸入姓名:<input type="text" value={name} onChange={(e)=>{
+      請輸入姓名:<input type="text" value={name} onChange={()=>{
         //console.log(e);
         setName(e.target.value);
-      }}/>  {name}
+      }}/>  
       {/* 使用value一定要有onChange={()=>{}},event=事件參數，縮寫e*/}
       <br/>
       請輸入姓名2:<input type="text" value={name2} onChange={changeName}/>{name2}  
