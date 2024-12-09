491af105436384de8a3f28e5fa1da6224260d753
season-123-docom <bobo10161995@gmail.com>
2024-12-09 12:12:22 +0800

    1209

---
+++ my-react-app1/src/App-weather-1204 copy.jsx
@@ -0,0 +1,47 @@
+//記得清空存檔，vite只能執行這個頁面的樣子
+
+import { useEffect } from "react";
+import axios from "axios";
+import "./style.css";
+import { IoUmbrella } from "react-icons/io5";
+
+function App() {
+  useEffect(() => {
+    (async () => {
+      // 本地端要在web-server伺服器下才可以執行
+      //const data = await axios.get("./F-C0032-001.json");
+
+      //json的連結必須是公開網址下的來源
+      const data = await axios.get(
+        "https://season-123-docom.github.io/myNewReact7/"
+      );
+      // console.log = (data)
+
+      const { location } = data.data.cwaopendata.dataset;
+      // console.log(location);
+
+      // const { location } = data.data.cwaopendata.dataset;
+      // const options = { hour: "numeric", minute: "numeric" };
+      // console.log(location);
+
+      //locationName => 縣市名
+      //elementName => Wx => 天氣概況
+      //elementName => PoP => 降雨率
+    })();
+  }, []);
+
+  return (
+    <>
+      <div className="wrap">
+        <h2>36小時天氣預報</h2>
+        <div className="container">
+          {
+            weather
+          }
+
+        </div>
+      </div>
+    </>
+  );
+}
+export default App;
